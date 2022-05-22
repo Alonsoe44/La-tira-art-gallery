a3377bc22d2f7b6ec0e5dac561e969097ca9efa0
Emiliano Polanco Baquerizo <emilianopolanco5@gmail.com>
2022-05-22 18:38:30 +0200

    Fixing footer import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
-import Footer from "./components/footer/footer";
+import Footer from "./components/footer/Footer";
+
 import NavigationBar from "./components/navigationBar/NavigationBar";
 import NavigationBarMobile from "./components/navigationBar/NavigationBarMobile";
 import HomePage from "./pages/HomePage";
